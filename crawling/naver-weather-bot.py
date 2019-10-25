5521a8417c84bb147874be3f6e4558bc6fdeec68
rudalson <rudalson@gmail.com>
2019-10-25 18:44:51 +0900

    added the naver weather info bot

---
+++ crawling/naver-weather-bot.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+Simple Bot to reply to Telegram messages.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+from bs4 import BeautifulSoup as bs
+import requests
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def mise(update, context):
+    """Send a message when the command /mise is issued."""
+
+    html = requests.get('https://search.naver.com/search.naver?query=날씨')
+    # pprint(html.text)
+
+    soup = bs(html.text, 'html.parser')
+
+    dust_data = soup.find('div', {'class': 'detail_box'}).findAll('dd')
+
+    fine_dust = dust_data[0].find('span', {'class': 'num'}).text
+    ultra_fine_dust = dust_data[1].find('span', {'class': 'num'}).text
+
+    location_data = soup.find('div', {'class': 'sort_box'}).find('em').text
+
+    reply_text = f'{location_data} - 미세먼지:{fine_dust}, 초미세먼지:{ultra_fine_dust}'
+
+    print(reply_text)
+    update.message.reply_text(reply_text)
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("171459707:AAEhK_H1INhRr6t2yJUvFF1o0D7ivZ1e-ME", use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mise", mise))
+
+    # on noncommand i.e message - echo the message on Telegram
+    # dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
